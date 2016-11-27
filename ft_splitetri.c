48898705cd8bafa7bd8b400d587b4b7ed68dd836
Morgane ALBANESE <malbanes@e1r9p13.42.fr>
2016-11-27 16:41:37 +0100

    ajout setalpha

---
+++ ft_splitetri.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_splitTetri.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbanes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/26 19:03:59 by malbanes          #+#    #+#             */
+/*   Updated: 2016/11/27 16:41:18 by malbanes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+static int		cntTetri(char *s, char c)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] == c && (s[i + 1] == c))
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+char		**ft_splitetri(char *s, char c)
+{
+	int		i;
+	int		y;
+	char	**split;
+	int		end;
+
+	i = 0;
+	y = 0;
+	end = 0;
+	if (!(split = (char**)malloc(sizeof(char*) * (cntTetri(s, c)) + 1))
+		|| (!s))
+		return (NULL);
+	while (y < cntTetri(s, c) && s[i]) //s[i] facultatif
+	{
+		split[y++] = ft_strsub(s, i , 20); // 21 pour ajouter le dernier \n
+		i += 21;
+	}
+	split[y] = 0;
+	return (split);
+}
--- open.c
+++ open.c
@@ -5,17 +5,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 546// 21 Char * 26 Tetrimax
 
+char	**ft_splitetri(char *s, char c);
 int		cntTetri(char *s, char c);
+void	ft_putstr(char *s);
+void	ft_setalpha(char **s);
 
 int		main()
 {
 	int fd;
 	int ret;
 	char buf[BUF_SIZE + 1];
+	char	**tabtetri;
+	int		y;
 
+	y = 0;
 	fd = open("tetritest.txt", O_RDONLY);
 	if (fd == -1)
 	{
@@ -24,12 +31,19 @@ int		main()
 	}
 	ret = read(fd, buf, BUF_SIZE); // soit boucle de read, soit taille max 26 elements
 	buf[ret] = '\0';
-	printf("%s",  buf);
 	if (close(fd) == -1)
 	{
 		printf("read FAILED");
 		return (1);
 	}
-		printf("%d Tetriminos", cntTetri(buf, '\n'));
+	tabtetri = ft_splitetri(buf, '\n');
+//	free(buf);
+	ft_setalpha(tabtetri);
+	while (tabtetri[y] != 0)
+	{
+		ft_putstr(tabtetri[y]);
+		ft_putstr("Tetri suivant\n");
+		y++;
+	}
 	return (0);
 }
